628b831fdd228c63625f07116ed63c31f70cc2ad
Kim Gonzales <Kimgo589@gmail.com>
2019-01-24 16:20:24 -0500

    add event helpers, reduce logic in event index view

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -9,4 +9,16 @@ module EventsHelper
     date.strftime("%B %Y")
   end
 
+  def date_range(start_date)
+    (start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week).to_a
+  end
+
+  def first_week_of_month(start_date)
+    date_range(start_date).slice(0,7)
+  end
+
+  def display_time(event)
+    event.time.strftime("%b %d, %Y %I:%M %p")
+  end
+
 end
